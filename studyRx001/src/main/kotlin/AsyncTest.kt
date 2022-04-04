6a0d95015c8864c72b75a220f329fb927f5f50e0
Yihui Shi <yihui.shi@outlook.com>
2022-04-03 17:12:16 -0700

    fix rx

--- studyRx001/src/main/kotlin/AsyncTest.kt
+++ studyRx001/src/main/kotlin/AsyncTest.kt
@@ -1,18 +1,22 @@
-import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.internal.schedulers.ComputationScheduler
-import io.reactivex.rxjava3.internal.schedulers.IoScheduler
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.kotlin.toObservable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class AsyncTest {
     fun startRStream() {
-        val list = listOf("1", "2", "3", "4", "5")
+        val list = listOf("1", "23", "345", "4567", "5")
 
         list.toObservable()
+            .map {it + "_suffix"}
+            .filter {it.length > 9}
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.computation())
             .subscribeBy(
                 onNext = { println(it) },
                 onError = { it.printStackTrace() },
                 onComplete = { println("onComplete!") }
             )
+
+        Thread.sleep(2000)
     }
 }
